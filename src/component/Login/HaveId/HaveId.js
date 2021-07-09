9c9d2cbaadd877f467cf942f019815a07a68f6f0
jangjaeko <jangjaeko45@gmail.com>
2021-07-09 18:27:54 +0900

    havid button edit

--- src/component/Login/HaveId/HaveId.js
+++ src/component/Login/HaveId/HaveId.js
@@ -8,6 +8,51 @@ import {
   TextInput,
 } from 'react-native';
 
+export default function HaveId(props) {
+  const [PasswordInput, setPasswordInput] = useState('');
+  const onChangeInput = event => {
+    setPasswordInput(event);
+  };
+  return (
+    <View style={styles.mainView}>
+      <Text style={{fontWeight: 'bold', paddingTop: 24}}>
+        이미 회원이시네요!
+      </Text>
+      <Text style={{fontWeight: 'bold'}}>비밀번호 입력</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="비밀번호를 입력하세요"
+        type="password"
+        value={PasswordInput}
+        onChangeText={onChangeInput}
+        secureTextEntry={true}></TextInput>
+
+      {PasswordInput === '' ? (
+        <View style={styles.ButtonDesign}>
+          <Button
+            title="완료"
+            color="#D6D7D9"
+            onPress={() => {
+              props.navigation.navigate('SignUp');
+            }}
+          />
+        </View>
+      ) : (
+        <View style={styles.ButtonDesign2}>
+          <Button
+            title="완료"
+            backgroundColor="#E17551"
+            color="white"
+            onPress={() => {
+              props.navigation.navigate('SignUp');
+            }}
+          />
+        </View>
+      )}
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   mainView: {
     backgroundColor: 'white',
@@ -41,29 +86,12 @@ const styles = StyleSheet.create({
     height: 48,
     justifyContent: 'center',
   },
+  ButtonDesign2: {
+    borderRadius: 10,
+    width: 160,
+    height: 48,
+    justifyContent: 'center',
+    marginTop: 50,
+    backgroundColor: '#E17551',
+  },
 });
-
-export default function HaveId(props) {
-  return (
-    <View style={styles.mainView}>
-      <Text style={{fontWeight: 'bold', paddingTop: 24}}>
-        이미 회원이시네요!
-      </Text>
-      <Text style={{fontWeight: 'bold'}}>비밀번호 입력</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="비밀번호를 입력하세요"
-        type="password"
-        secureTextEntry={true}></TextInput>
-      <View style={styles.ButtonDesign}>
-        <Button
-          title="완료"
-          color="#D6D7D9"
-          onPress={() => {
-            props.navigation.navigate('SignUp');
-          }}
-        />
-      </View>
-    </View>
-  );
-}
