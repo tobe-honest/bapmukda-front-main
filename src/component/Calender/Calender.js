9906563b90000cd8a2a56d6cf09774558a873c91
jangjaeko <jangjaeko45@gmail.com>
2021-07-18 21:53:36 +0900

    add modal module

--- src/component/Calender/Calender.js
+++ src/component/Calender/Calender.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Button, Text, StyleSheet} from 'react-native';
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 import moment from 'moment';
+import Modal from 'react-native-modal';
 export default function Calender() {
   const vacation = {key: 'vacation', color: 'red', selectedDotColor: 'blue'};
   const massage = {key: 'massage', color: 'blue', selectedDotColor: 'blue'};
   const workout = {key: 'workout', color: 'green'};
   const today = moment().format('YYYY-MM-DD');
-
+  const [isModalVisible, setModalVisible] = useState(false);
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
   return (
     <View>
       <Calendar
@@ -21,7 +25,28 @@ export default function Calender() {
           '2021-07-17': {dots: [massage, workout]},
           [today]: {selected: true, selectedColor: 'gray'},
         }}
+        onDayPress={toggleModal}
       />
+      <Modal
+        isVisible={isModalVisible}
+        style={{justifyContent: 'flex-end', margin: 0}}
+        transparent={true}
+        coverScreen={false}
+        backdropColor={'transparent'}
+        backdropOpacity={1}>
+        <View
+          style={{
+            flex: 0.5,
+            backgroundColor: 'white',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            height: '30%',
+          }}>
+          <Text>Hello!</Text>
+
+          <Button title="Hide modal" onPress={toggleModal} />
+        </View>
+      </Modal>
       <Agenda
         items={{
           '2021-07-18': [{name: 'item 1 - any js object'}],
@@ -30,3 +55,11 @@ export default function Calender() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  modalStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+});
